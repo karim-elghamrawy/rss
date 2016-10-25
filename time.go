d29424801a8119499f948f3a0ed5a6993f5065eb
karim-elghamrawy <elghamrawyk@vmware.com>
2016-10-24 23:28:36 -0700

    added another time layout

--- time.go
+++ time.go
@@ -12,6 +12,7 @@ import (
 var TimeLayouts = []string{
 	"Mon, _2 Jan 2006 15:04:05 MST",
 	"Mon, _2 Jan 2006 15:04:05 -0700",
+	"Mon, 2 Jan 2006 15:04 MST",
 	time.ANSIC,
 	time.UnixDate,
 	time.RubyDate,
