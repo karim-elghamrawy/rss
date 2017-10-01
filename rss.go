4074701036910123ac3326b1167164bd56ca9410
kreamyx <karim.y.elghamrawy@gmail.com>
2017-10-01 00:16:14 -0700

    more debugging

--- rss.go
+++ rss.go
@@ -14,19 +14,16 @@ import (
 
 // Parse RSS or Atom data.
 func Parse(data []byte) (*Feed, error) {
-	if strings.Contains(string(data), "atom:link") {
-		fmt.Println("[i] Parsing as Atom")
-		return parseAtom(data, database)
-	}
 	if strings.Contains(string(data), "<rss") {
 		fmt.Println("[i] Parsing as RSS 2.0")
 		return parseRSS2(data, database)
-	}
-	if strings.Contains(string(data), "xmlns=\"http://purl.org/rss/1.0/\"") {
+	} else if strings.Contains(string(data), "xmlns=\"http://purl.org/rss/1.0/\"") {
 		fmt.Println("[i] Parsing as RSS 1.0")
 		return parseRSS1(data, database)
+	} else {
+		fmt.Println("[i] Parsing as Atom")
+		return parseAtom(data, database)
 	}
-	return nil, errors.New("not a proper syndication file")
 }
 
 // CacheParsedItemIDs enables or disable Item.ID caching when parsing feeds.
@@ -175,7 +172,6 @@ type Item struct {
 	Summary    string       `json:"summary"`
 	Content    string       `json:"content"`
 	Link       string       `json:"link"`
-	AtomLink   string       `json:"atom:link"`
 	Date       time.Time    `json:"date"`
 	ID         string       `json:"id"`
 	Enclosures []*Enclosure `json:"enclosures"`
--- rss_2.0.go
+++ rss_2.0.go
@@ -70,13 +70,13 @@ func parseRSS2(data []byte, read *db) (*Feed, error) {
 
 	// Process items.
 	for _, item := range channel.Items {
-		if strings.Trim(item.Link, " ") == "" && item.AtomLink != "" {
+		if strings.Trim(item.Link, " ") == "" {
 			fmt.Println("Empty Item Link")
-			fmt.Printf("AtomLink %s", item.AtomLink)
-			item.Link = item.AtomLink
+			fmt.Printf("AtomLink %+v", item.AtomLink)
+			item.Link = item.AtomLink[0].Href
 		} else {
 			fmt.Printf("ItemLink %s\n", item.Link)
-			fmt.Printf("AtomLink %s\n", item.AtomLink)
+			fmt.Printf("AtomLink %+v\n", item.AtomLink)
 		}
 		if item.ID == "" {
 			if item.Link == "" {
@@ -181,7 +181,7 @@ type rss2_0Item struct {
 	Description string            `xml:"description"`
 	Content     string            `xml:"encoded"`
 	Link        string            `xml:"link"`
-	AtomLink    string            `xml:"atom:link"`
+	AtomLink    []atomLink        `xml:"atom:link"`
 	PubDate     string            `xml:"pubDate"`
 	Date        string            `xml:"date"`
 	ID          string            `xml:"guid"`
